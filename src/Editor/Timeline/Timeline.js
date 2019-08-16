b44362b3c63c6762e1d7f003dd0386b00c758591
Bryce Eklund <bryce.eklund@gmail.com>
2019-08-16 16:18:18 -0700

    add state to timeline component

--- src/Editor/Timeline/Timeline.js
+++ src/Editor/Timeline/Timeline.js
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import './Timeline.css';
 
 class Timeline extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeChords: [0, 1, 2, 3, 4, 5, 6],
+        init: 0,
+        end: 4,//this.state.activeChords.length < 5 ? this.state.activeChords.length : 4,
+        timelineChords: [0, 1, 2, 3, 4, 5, 6].slice(this.init, this.end)
+      }
+    }
+    incrementTimeline = () => {
+      this.setState({
+        init: this.state.init++,
+        end: this.state.end++
+      })
+    }
+    decrementTimeline = () =>  {
+      this.setState({
+        init: this.state.init--,
+        end: this.state.end--
+      })
+    }
     render() {
         return (
             <section className='timeline'>
@@ -12,7 +33,7 @@ class Timeline extends Component {
             <div className='player_timeline'>
               <div className='chord_box'>
                 <button className='delete_chord'>X</button>
-                <span className='chord_label'>G</span>
+                <span className='chord_label'>{this.state.timelineChords[0]}</span>
                 <div className='move_chord_buttons'>
                   <button className='move_chord_left'><img src='' alt='move left' /></button>
                   <button className='move_chord_right'><img src='' alt='move right' /></button>
@@ -20,7 +41,7 @@ class Timeline extends Component {
               </div>
               <div className='chord_box'>
                 <button className='delete_chord'>X</button>
-                <span className='chord_label'>B</span>
+                <span className='chord_label'>{this.state.timelineChords[1]}</span>
                 <div className='move_chord_buttons'>
                   <button className='move_chord_left'><img src='' alt='move left' /></button>
                   <button className='move_chord_right'><img src='' alt='move right' /></button>
@@ -28,7 +49,7 @@ class Timeline extends Component {
               </div>
               <div className='chord_box'>
                 <button className='delete_chord'>X</button>
-                <span className='chord_label'>Em</span>
+                <span className='chord_label'>{this.state.timelineChords[2]}</span>
                 <div className='move_chord_buttons'>
                   <button className='move_chord_left'><img src='' alt='move left' /></button>
                   <button className='move_chord_right'><img src='' alt='move right' /></button>
@@ -36,7 +57,7 @@ class Timeline extends Component {
               </div>
               <div className='chord_box'>
                 <button className='delete_chord'>X</button>
-                <span className='chord_label'>C</span>
+                <span className='chord_label'>{this.state.timelineChords[3]}</span>
                 <div className='move_chord_buttons'>
                   <button className='move_chord_left'><img src='' alt='move left' /></button>
                   <button className='move_chord_right'><img src='' alt='move right' /></button>
@@ -44,7 +65,7 @@ class Timeline extends Component {
               </div>
             </div>
             <div className='scrubber_buttons'>
-              <button><img src='' alt='scrub left' /></button>
+              <button onClick={() => this.decrementTimeline}><img src='' alt='scrub left' /></button>
               <div className="slide_container">
                 <label for='volume'>Volume</label>
                 <input type="range" min="1" max="100" value="50" className="volume_slider" id="volume" />
@@ -53,7 +74,7 @@ class Timeline extends Component {
                 <label for='speed'>Speed</label>
                 <input type="range" min="1" max="100" value="50" className="speed_slider" id="speed" />
               </div>
-              <button><img src='' alt='scrub right' /></button>
+              <button onClick={() => this.incrementTimeline}><img src='' alt='scrub right' /></button>
             </div>
           </section>
         );
